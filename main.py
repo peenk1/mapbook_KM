03bfb5c8d64cef94904f498910c241d826f20212
peenk1 <mastalski102@gmail.com>
2025-11-02 22:26:09 +0100

    w&p: added a list with users, locations and posts

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ zmienna_2:str = 'Michasia'
 zmienna_3:str = 'Zdzisia'
 zmienna_4:str = 'Mateusz'
 
-uzytkownicy =[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
-for zmienna in uzytkownicy:
-    print(f'Hello {zmienna}')
+
+users:list = [
+    {'name':zmienna_1, 'location': 'Washington', 'posts': 67},
+    {'name':zmienna_2, 'location': 'Paris', 'posts': 22},
+    {'name':zmienna_3, 'location': 'Michigan', 'posts': 55},
+    {'name':zmienna_4, 'location': 'Warsaw', 'posts': 3}
+    ]
+
+for user in users:
+    print(f'Your user {user['name']}, from {user['location']}, has {user['posts']} posts.')
