13f1d4468b6a68f62fefd52385a8b167527a28c7
peenk1 <mastalski102@gmail.com>
2025-11-02 22:24:33 +0100

    w&p: added for loop with users

---
+++ main.py
@@ -0,0 +1,8 @@
+zmienna_1:str = 'Kasia'
+zmienna_2:str = 'Michasia'
+zmienna_3:str = 'Zdzisia'
+zmienna_4:str = 'Mateusz'
+
+uzytkownicy =[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for zmienna in uzytkownicy:
+    print(f'Hello {zmienna}')
